9bf361eb6c9a9b2baeabc4bbff648d20c2b094e1
Arne Kiesewetter <banane9@live.de>
2022-08-15 02:47:00 +0200

    Add notifications for unfocused worlds and a bunch of settings. Untested.

--- UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
+++ UserJoinLeaveNotifications/UserJoinLeaveNotifications.cs
@@ -4,98 +4,146 @@ using NeosModLoader;
 using System;
 using BaseX;
 using System.Reflection;
-
 using System.Threading.Tasks;
+using System.Collections;
+using FrooxEngine.UIX;
 
 namespace UserJoinLeaveNotifications
 {
     public class UserJoinLeaveNotifications : NeosMod
     {
+        public static ModConfiguration Config;
+
+        private static readonly MethodInfo addNotification = AccessTools.Method(typeof(NotificationPanel), "AddNotification", new Type[] { typeof(string), typeof(string), typeof(Uri), typeof(color), typeof(string), typeof(Uri), typeof(IAssetProvider<AudioClip>) });
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> FocusWorldFromNotifications = new ModConfigurationKey<bool>("FocusWorldFromNotifications", "Makes clicking a User join/leave notification focus the world it relates to.", () => true);
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> JoinFocusedWorldColor = new ModConfigurationKey<color>("JoinFocusedWorldColor", "Color of the notification for a User joining the focused world.", () => BlendColor(color.Blue));
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> JoinUnfocusedWorldColor = new ModConfigurationKey<color>("JoinUnfocusedWorldColor", "Color of the notification for a User joining an unfocused world.", () => BlendColor(BlendColor(color.Blue)));
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> LeaveFocusedWorldColor = new ModConfigurationKey<color>("LeaveFocusedWorldColor", "Color of the notification for a User leaving the focused world.", () => BlendColor(color.Red));
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<color> LeaveUnfocusedWorldColor = new ModConfigurationKey<color>("LeaveUnfocusedWorldColor", "Color of the notification for a User leaving an unfocused world.", () => BlendColor(BlendColor(color.Red)));
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> ShowUnfocusedWorldEvents = new ModConfigurationKey<bool>("ShowUnfocusedWorldEvents", "Color of the notification for a User leaving an unfocused world.", () => true);
+
+        public override string Author => "Banane9";
+        public override string Link => "https://github.com/Banane9/NeosUserJoinLeaveNotifications";
         public override string Name => "UserJoinLeaveNotifications";
-        public override string Author => "badhaloninja";
-        public override string Version => "1.2.0";
-        public override string Link => "https://github.com/badhaloninja/UserJoinLeaveNotifications";
-        public override void OnEngineInit()
-        {
-            Engine.Current.RunPostInit(Setup);
-        }
-        
+        public override string Version => "2.0.0";
 
-        private static MethodInfo addNotification;
-
-        private static UserBag currentUserBag;
-        
-        
         public static void Setup()
         {
             // Hook into the world focused event
-            Engine.Current.WorldManager.WorldFocused += OnWorldFocused;
+            Engine.Current.WorldManager.WorldAdded += OnWorldAdded;
+        }
 
-            // Store the method info for later use
-            addNotification = AccessTools.Method(typeof(NotificationPanel), "AddNotification", new Type[] { typeof(string), typeof(string), typeof(Uri), typeof(color), typeof(string), typeof(Uri), typeof(IAssetProvider<AudioClip>) });
-        }
-        private static void OnWorldFocused(World focusedWorld)
+        public override void OnEngineInit()
         {
-            // User join leave events only trigger as host
-            if (currentUserBag != null)
-            { // Remove the event handler from the old world
-                currentUserBag.OnElementAdded -= OnUserJoined;
-                currentUserBag.OnElementRemoved -= OnUserLeft;
-            }
-            
-            
-            // Store the user bag of the new world
-            currentUserBag = GetUserbag(focusedWorld);
-            // Add the event handler to the new world
-            currentUserBag.OnElementAdded += OnUserJoined;
-            currentUserBag.OnElementRemoved += OnUserLeft;
+            Config = GetConfiguration();
+            Config.Save(true);
+
+            Engine.Current.RunPostInit(Setup);
         }
+
+        private static void AddNotification(World world, string userId, string message, Uri thumbnail, color backgroundColor, string mainMessage = "N/A", Uri overrideProfile = null, IAssetProvider<AudioClip> clip = null)
+        {
+            NotificationPanel.Current?.RunSynchronously(() =>
+            { // ;-;
+                addNotification.Invoke(NotificationPanel.Current, new object[] { userId, message, thumbnail, backgroundColor, mainMessage, overrideProfile, clip });
+                AddWorldFocus(NotificationPanel.Current, world);
+            });
+        }
+
+        private static void AddWorldFocus(NotificationPanel notificationPanel, World world)
+        {
+            if (!Config.GetValue(FocusWorldFromNotifications))
+                return;
+
+            // _items is List<NotificationPanel.NotificationItem>
+            var items = Traverse.Create(notificationPanel).Field("_items").GetValue<IList>();
+            var root = Traverse.Create(items[items.Count - 1]).Field("root").GetValue<Slot>();
+
+            root.GetComponentInChildren<Button>().LocalPressed += (button, data) => Engine.Current.WorldManager.FocusWorld(world);
+        }
+
+        private static color BlendColor(color color)
+        {
+            return MathX.Lerp(color, color.White, 0.5f);
+        }
+
+        private static UserBag GetUserbag(World world)
+        {
+            return Traverse.Create(world).Field<UserBag>("_users").Value;
+        }
+
+        private static string GetUserChangeMessage(User user, bool joining, World world, bool focusedWorld)
+        {
+            return $"{user.UserName} {(joining ? "joined" : "left")}{(Config.GetValue(ShowUnfocusedWorldEvents) ? $" {(focusedWorld ? "this Session" : world.Name)}" : "")}";
+        }
+
+        // Async method to fetch thumbnail from user id
+        private static async Task<Uri> GetUserThumbnail(string userId)
+        {
+            // Handle fetching profile, AddNotification only gets profile data for friends
+            var cloudUserProfile = (await Engine.Current.Cloud.GetUser(userId))?.Entity?.Profile;
+            return CloudX.Shared.CloudXInterface.TryFromString(cloudUserProfile?.IconUrl) ?? NeosAssets.Graphics.Thumbnails.AnonymousHeadset;
+        }
+
         private static void OnUserJoined(SyncBagBase<RefID, User> bag, RefID key, User user, bool isNew)
         {
-            if (user.IsLocalUser) return;
-            if (NotificationPanel.Current == null) return;
-            
+            var focusedWorld = Engine.Current.WorldManager.FocusedWorld == bag.World;
+
+            if (user.IsLocalUser || NotificationPanel.Current == null
+            || (!Config.GetValue(ShowUnfocusedWorldEvents) && !focusedWorld))
+                return;
+
             NotificationPanel.Current.RunInUpdates(3, async () =>
             { // Running immediately results in the getuser to return a BadRequest
-                Uri thumbnail = await GetUserThumbnail(user.UserID);
-                
-                AddNotification(string.Format("{0} joined", user.UserName),MathX.Lerp(color.Blue, color.White, 0.5f), "User Joined", thumbnail);
+                var thumbnail = await GetUserThumbnail(user.UserID);
+
+                AddNotification(bag.World,
+                    user.UserID,
+                    GetUserChangeMessage(user, true, bag.World, focusedWorld),
+                    thumbnail,
+                    Config.GetValue(focusedWorld ? JoinFocusedWorldColor : JoinUnfocusedWorldColor),
+                    $"User Joined{(focusedWorld ? "" : " Unfocused")}");
             });
         }
 
         private static async void OnUserLeft(SyncBagBase<RefID, User> bag, RefID key, User user)
         {
-            if (user.IsLocalUser) return;
-            if (NotificationPanel.Current == null) return;
-            
-            Uri thumbnail = await GetUserThumbnail(user.UserID);
-            AddNotification(string.Format("{0} left", user.UserName), MathX.Lerp(color.Red, color.White, 0.5f), "User Left", thumbnail);
+            var focusedWorld = Engine.Current.WorldManager.FocusedWorld == bag.World;
+
+            if (user.IsLocalUser || NotificationPanel.Current == null
+            || (!Config.GetValue(ShowUnfocusedWorldEvents) && !focusedWorld))
+                return;
+
+            var thumbnail = await GetUserThumbnail(user.UserID);
+
+            AddNotification(bag.World,
+                user.UserID,
+                GetUserChangeMessage(user, false, bag.World, focusedWorld),
+                thumbnail,
+                Config.GetValue(focusedWorld ? LeaveFocusedWorldColor : LeaveUnfocusedWorldColor),
+                $"User Left{(focusedWorld ? "" : " Unfocused")}");
         }
 
-        
-        
-        // Async method to fetch thumbnail from user id
-        private static async Task<Uri> GetUserThumbnail(string userId)
+        private static void OnWorldAdded(World world)
         {
-            var cloudUserProfile = (await Engine.Current.Cloud.GetUser(userId))?.Entity?.Profile;
-            // Handle fetching profile, AddNotification only gets profile data for friends
-            var thumbnail = CloudX.Shared.CloudXInterface.TryFromString(cloudUserProfile?.IconUrl) ?? NeosAssets.Graphics.Thumbnails.AnonymousHeadset;
-            return thumbnail;
-        }
-        
-        private static UserBag GetUserbag(World world)
-        { // Return the user bag field
-            return Traverse.Create(world).Field("_users").GetValue<UserBag>();
-        }
-        
-        private static void AddNotification(string message, color backgroundColor, string mainMessage, Uri overrideProfile)
-        {
-            // Not using Show Notification because it does not expose main message
-            if (NotificationPanel.Current == null) return;
-            NotificationPanel.Current.RunSynchronously(() =>
-            { // ;-;
-                addNotification.Invoke(NotificationPanel.Current, new object[] { null, message, null, backgroundColor, mainMessage, overrideProfile, null });
-            });
+            // Store the user bag of the new world
+            var userBag = GetUserbag(world);
+
+            // Add the event handler to the new world
+            userBag.OnElementAdded += OnUserJoined;
+            userBag.OnElementRemoved += OnUserLeft;
         }
     }
 }
